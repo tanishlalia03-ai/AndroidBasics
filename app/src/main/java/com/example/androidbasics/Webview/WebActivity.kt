7e82076aa621ba184e7b20dbaebef331fa13d9d2
tanishlalia03-ai <tanishlalia03@gmail.com>
2026-02-05 09:01:11 +0530

    web view

---
+++ app/src/main/java/com/example/androidbasics/Webview/WebActivity.kt
@@ -0,0 +1,44 @@
+package com.example.androidbasics.Webview
+
+import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidbasics.R
+import com.example.androidbasics.databinding.ActivityWebBinding
+
+class WebActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityWebBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+        binding= ActivityWebBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.myWebView.settings.javaScriptEnabled = true
+        binding.myWebView.webViewClient = WebViewClient()
+        binding.myWebView.loadUrl("https://huggingface.co/")
+    }
+
+
+    override fun onBackPressed() {
+
+        if(binding.myWebView.canGoBack()){
+            binding.myWebView.goBack()
+        }
+
+        else {
+            super.onBackPressed()
+        }
+    }
+}
