72f4ab2cb1984ff163968ac6d1dba0d6121a893e
tanishlalia03-ai <tanishlalia03@gmail.com>
2026-02-05 17:57:55 +0530

    Update WebActivity.kt

--- app/src/main/java/com/example/androidbasics/Webview/WebActivity.kt
+++ app/src/main/java/com/example/androidbasics/Webview/WebActivity.kt
@@ -2,10 +2,12 @@ package com.example.androidbasics.Webview
 
 import android.os.Bundle
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.room.RoomDatabase
 import com.example.androidbasics.R
 import com.example.androidbasics.databinding.ActivityWebBinding
 
@@ -16,29 +18,31 @@ class WebActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-
-        binding= ActivityWebBinding.inflate(layoutInflater)
+        binding = ActivityWebBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        // 1. Setup WebView
         binding.myWebView.settings.javaScriptEnabled = true
         binding.myWebView.webViewClient = WebViewClient()
         binding.myWebView.loadUrl("https://huggingface.co/")
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.myWebView.canGoBack()) {
+                    binding.myWebView.goBack()
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, callback)
     }
 
-
-    override fun onBackPressed() {
-
-        if(binding.myWebView.canGoBack()){
-            binding.myWebView.goBack()
-        }
-
-        else {
-            super.onBackPressed()
-        }
-    }
 }
