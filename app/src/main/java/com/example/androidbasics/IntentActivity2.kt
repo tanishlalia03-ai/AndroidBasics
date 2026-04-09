4e3bb47d13fc032b622b3d5f0453d936b67d0b0a
tanishlalia03-ai <tanishlalia03@gmail.com>
2026-04-09 08:56:36 +0530

    done

--- app/src/main/java/com/example/androidbasics/IntentActivity2.kt
+++ app/src/main/java/com/example/androidbasics/IntentActivity2.kt
@@ -8,8 +8,6 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class IntentActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
